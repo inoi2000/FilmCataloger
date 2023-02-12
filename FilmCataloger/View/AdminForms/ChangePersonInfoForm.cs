b2906d035b3fff43ade567b1bb887a2b71039745
inoi2000 <nikolaiklonov@yandex.ru>
2023-02-12 19:10:12 +0500

    Adding Forms

---
+++ FilmCataloger/View/AdminForms/ChangePersonInfoForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmCataloger.View.AdminForms
+{
+    public partial class ChangePersonInfoForm : Form
+    {
+        public ChangePersonInfoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
